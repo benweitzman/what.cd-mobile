10c38c8bebe2373f5a5977b99c4f5ddf85008260
Jim Rollenhagen <jim@jimrollenhagen.com>
2012-11-27 17:35:13 -0800

    initial setup of router and whatnot

--- js/app.js
+++ js/app.js
@@ -4,11 +4,26 @@ App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
   templateName: 'application'
 });
+App.IndexView = Ember.View.extend({
+  templateName: 'index'
+});
+App.LoginFormView = Ember.View.extend({
+  templateName: 'login-form'
+});
 
 App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('index');
+      }
+    }),
+    login: Ember.Route.extend({
+      route: '/login',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('loginForm');
+      }
     })
   })
 })
