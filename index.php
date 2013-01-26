324a4d46390e28f81626ace348ee13dc90be8345
Twinklebear <willusher.life@gmail.com>
2013-01-26 09:03:37 -0800

    move script includes to bottom of body

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<title>What CD Angular</title>
-	<script type="text/javascript" src="js/libs/jquery.min.js"></script>
-	<script type="text/javascript" src="js/libs/angular.min.js"></script>
-	<script type="text/javascript" src="js/libs/bootstrap.min.js"></script>
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
-	<script type="text/javascript" src="js/app.js"></script>
-	<script type="text/javascript" src="js/services.js"></script>
+
 </head>
 
 <body ng-app="what-mobile">
 	<div ng-view></div>
+
+	<script type="text/javascript" src="js/libs/jquery.min.js"></script>
+	<script type="text/javascript" src="js/libs/angular.min.js"></script>
+	<script type="text/javascript" src="js/libs/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript" src="js/services.js"></script>
 </body>
 </html>
